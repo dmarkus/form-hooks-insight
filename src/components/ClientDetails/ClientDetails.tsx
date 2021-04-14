1f9defd1a40f8231ee0bbb9e649c7e8f142986a4
dmarkus <mar.dalka@gmail.com>
2021-04-14 05:31:20 +0200

    create client form

--- src/components/ClientDetails/ClientDetails.tsx
+++ src/components/ClientDetails/ClientDetails.tsx
@@ -1,2 +1,43 @@
 import * as React from "react";
-export const ClientDetails = () => <>Client Data</>;
+import { useForm, useWatch } from "react-hook-form";
+import { Client } from "../../types/client";
+
+export const ClientDetails = () => {
+  const {
+    control,
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Client>();
+
+  const onSubmit = handleSubmit((data) => console.log(data));
+  const firstName = useWatch({
+    control,
+    name: ["firstName", "lastName", ], // without supply name will watch the entire form, or ['firstName', 'lastName'] to watch both
+  });
+
+  console.log("firstName", firstName);
+  return (
+    <form onSubmit={onSubmit}>
+      <label>First Name</label>
+      <input {...register("firstName")} maxLength={50} />
+      <label>Last Name</label>
+      <input {...register("lastName")} maxLength={50} />
+        <select {...register("gender")}>
+            <option value="female">female</option>
+            <option value="male">male</option>
+        </select>
+
+      <button
+        type="button"
+        onClick={() => {
+          setValue("firstName", "luo");
+          console.log(errors);
+        }}
+      >
+        SetValue
+      </button>
+    </form>
+  );
+};
